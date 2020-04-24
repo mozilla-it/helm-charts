c4e8643193b5a33cde302eaeeb68ab3ac427928e
limed <limed@mozilla.com>
2020-04-24 08:34:33 -0700

    remove variable thats not being used

--- ci/lint.sh
+++ ci/lint.sh
@@ -9,7 +9,6 @@ DEFAULT_IMAGE=quay.io/helmpack/chart-testing:v3.0.0-rc.1
 main() {
     local image="$DEFAULT_IMAGE"
     local changed
-    local command=
 
     run_ct_container
     trap cleanup EXIT
