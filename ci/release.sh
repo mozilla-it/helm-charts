167df2ad4d3c327184c3e5db4950c37907772482
limed <limed@mozilla.com>
2020-04-24 09:31:18 -0700

    Fix release script

--- ci/release.sh
+++ ci/release.sh
@@ -4,16 +4,22 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-mkdir release
+readonly REPO_ROOT=$(git rev-parse --show-toplevel)
 
-cd charts
+readonly CHARTS_DIR="${REPO_ROOT}/charts"
+readonly RELEASES_DIR="${REPO_ROOT}/releases"
+
+mkdir -p "${RELEASES_DIR}"
+
+cd "${CHARTS_DIR}"
 for i in $(find . -maxdepth 1 -mindepth 1 -type d) ; do
-    helm lint $i
-    helm dependency update $i
-    helm package $i -d ../releases
+    helm lint "${i}"
+    helm dependency update "${i}"
+    helm package "${i}" -d "${RELEASES_DIR}"
 done
 cd ..
 
 git checkout gh-pages
-mv *.tgz releases/
+mv ./*.tgz "${RELEASES_DIR}"
+cd "${RELEASES_DIR}"
 helm repo index --url https://mozilla-it.github.io/helm-charts/ --merge ../index.yaml .
