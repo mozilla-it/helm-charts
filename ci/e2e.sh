4a2584bdc555f032f6c3e4bd7259f74f97eaf990
Alberto del Barrio <adelbarrio@mozilla.com>
2020-07-16 15:47:04 +0200

    Allow Travis to Pull Containers from ECR

--- ci/e2e.sh
+++ ci/e2e.sh
@@ -4,6 +4,7 @@ readonly REPO_ROOT=$(git rev-parse --show-toplevel)
 readonly FUNCTIONS="${REPO_ROOT}/ci/functions.sh"
 readonly CLUSTER_NAME="${CLUSTER_NAME:-kind}"
 readonly CONFIG_FILE="${REPO_ROOT}/ci/kind.yaml"
+readonly NAMESPACE="chart-ci-e2e"
 
 # shellcheck source=ci/functions.sh
 [ -f "${FUNCTIONS}" ] && source "${FUNCTIONS}" || exit 1
@@ -15,13 +16,24 @@ create_kind_cluster() {
         --name "${CLUSTER_NAME}" \
         --config "${CONFIG_FILE}" \
         --wait 60s
+}
 
+configure_kind_cluster() {
     configure_kube
 
     kubectl cluster-info
     echo
 
     kubectl get nodes
+    echo
+    
+    if [[ $(kubectl get namespaces | grep -w $NAMESPACE) ]] ; then
+        # If the NS exists, delete it and recreate it to get a clean state
+        kubectl delete namespace $NAMESPACE
+        kubectl create namespace $NAMESPACE
+    else
+        kubectl create namespace $NAMESPACE
+    fi
     echo
 
     echo 'Cluster ready..'
@@ -40,15 +52,45 @@ configure_kube() {
 
 install_charts() {
     echo 'Installing charts...'
-    docker_exec ct install
+    docker_exec ct install --namespace $NAMESPACE
     echo
+}
+
+check_cluster_exists () {
+    # 0 if cluster already exists
+    kind get clusters | grep -q -w "$CLUSTER_NAME"
+    echo $?
+}
+
+create_ecr_secret () {
+    # Gets a valid token to pull from ECR and creates a secret with it
+    ACCOUNT=$(aws sts get-caller-identity --output text --query Account)
+    REGION=us-west-2
+    SECRET_NAME=ecr-registry
+    EMAIL=itse@mozilla.com
+
+    # Fetch token (which will expire in 12 hours)
+    TOKEN=$(aws ecr --region=$REGION get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2)
+
+    # Create or replace secret
+    kubectl delete secret -n $NAMESPACE --ignore-not-found "$SECRET_NAME"
+    kubectl create secret -n $NAMESPACE docker-registry "$SECRET_NAME" \
+     --docker-server="https://${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com" \
+     --docker-username=AWS \
+     --docker-password="${TOKEN}" \
+     --docker-email="${EMAIL}"
 }
 
 main() {
     run_ct_container
     trap cleanup EXIT
 
-    create_kind_cluster
+    if [[ $(check_cluster_exists) != 0 ]] ; then
+        create_kind_cluster
+    fi
+
+    configure_kind_cluster
+    create_ecr_secret
     install_charts
 }
 
