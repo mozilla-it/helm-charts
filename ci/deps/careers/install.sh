f39a88942aa14f31f9e6f41dbab598aea8f04d9e
Alan Alexander <duallain@mozilla.com>
2021-08-31 09:18:13 -0700

    initial commit of careers

---
+++ ci/deps/careers/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+
+readonly NAMESPACE="chart-ci-e2e"
+
+
+
+helm upgrade --install \
+  --version 10.4.2 \
+  --namespace $NAMESPACE \
+  --set postgresqlPassword=defaultpassword \
+  --set postgresqlUsername=careers \
+  --set postgresqlDatabase=careers \
+  --set fullnameOverride=postgres \
+  postgresql bitnami/postgresql
